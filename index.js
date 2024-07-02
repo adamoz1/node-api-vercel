8861d98bc54b7156df0e1c66b9445f3c1f4ae872
adamoz1 <jupiterleague8@gmail.com>
2024-07-02 16:30:37 +0530

    Added new downloadApk endpoint

--- index.js
+++ index.js
@@ -7,4 +7,8 @@ app.get('/getBuildNo',(req, res)=>{
     res.json({'build_no': BUILD_NO});
 })
 
+app.get('/getApk',(req,res)=>{
+    res.sendFile(__dirname+'/app-release.apk');
+})
+
 app.listen(3000);
