6c3fa06d5b93208e561d148b7c297d45a926015b
adamoz1 <jupiterleague8@gmail.com>
2024-07-02 13:56:23 +0530

    Trying to change the build no

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-var BUILD_NO = 0.01;
+var BUILD_NO = 0.02;
 
 app.get('/getBuildNo',(req, res)=>{
     res.json({'build_no': BUILD_NO});
