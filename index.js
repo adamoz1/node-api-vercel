d660b238d9e0b1e9595277579d71d488c56254da
adamoz1 <167079270+adamoz1@users.noreply.github.com>
2025-01-27 21:59:03 +0530

    Update for reverse list of purchase and double qty to int

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-var BUILD_NO = '2.1.0';
+var BUILD_NO = '2.1.1';
 
 app.get('/getBuildNo',(req, res)=>{
     res.json({'build_no': BUILD_NO});
