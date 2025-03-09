8616afa8f3ead74d6c777818c45a7aff3ade10a8
adamoz1 <167079270+adamoz1@users.noreply.github.com>
2025-03-09 11:53:47 +0530

    Updated build no

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-var BUILD_NO = '2.1.2';
+var BUILD_NO = '2.1.3';
 
 app.get('/getBuildNo',(req, res)=>{
     res.json({'build_no': BUILD_NO});
